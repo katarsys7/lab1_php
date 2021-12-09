d57c810cb230e236c38cdc9bb55c08746e5a4747
katarsys7 <94797649+katarsys7@users.noreply.github.com>
2021-12-09 14:37:58 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<?php
-	//Если город пустой - покажем форму
-	if (isset($_REQUEST['city'])) {
-?>
-		<form action="" method="GET">
-			<input type="text" name="name">
-			<input type="submit">
-		</form>
-<?php
-	}
-?>
-
-<?php
-	//Если форма была отправлена и город не пустой:
-	if (isset($_REQUEST['city'])) {
-		$city = strip_tags($_REQUEST['age']);
-		echo 'Ваш город: '.$age;
-	}
-?>
+<form action="" method="get">
+	<input type="text">
+	<input type="submit" value="отправить">
+</form>
