e5af0eb889eec4604819486ad82c0477c4e1024f
katarsys7 <94797649+katarsys7@users.noreply.github.com>
2022-04-02 08:24:58 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<html> <head>  <title>Тестируем PHP</title> </head> <body> <?php 
-	echo '<p>Привет, мир!</p>'; 
-?> </body></html>
+<html>
+	<head>
+		<title>Тестируем PHP</title>
+	</head>
+	<body>
+		<?php 
+			echo '<p>Привет, мир!</p>'; 
+		?>
+	</body>
+</html>
